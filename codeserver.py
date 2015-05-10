d5cca06c9ffdee0a128b63966c5166ab8e97aeac
Mohammad Moghaddam <moghaddam22@gmail.com>
2015-05-10 14:29:45 -0700

    trying to solve the conflicts between exec and eval conflicts in get_val; two important issues remain: functions does not have access to the local namesapce of clients, and line recognition does not work properly for several line client codes like function defs

--- codeserver.py
+++ codeserver.py
@@ -7,38 +7,61 @@ def codepool(codelist):
     return "".join(codestring)    
 
 def codespace(codestring):
+    """ given a list of code strings, returns the namespace dictionary of 
+    the executed codes within codestring
+    """
     dict = {}
     exec(codestring) in dict
     return dict
+
+def init_locals(clients):
+    dict = {}
+    for client in clients: dict[client] = {}
+    return dict
+
+import re
+pattern = re.compile(r".*import", re.DOTALL)
 
 class codeserver():
     """
     a codeserver object, gets initialized by a list of string names
     of importable python modules; They serve as our codespace.
-    A codeserver object, is able to evaluate strings of
-    expressions within the namespace of each code module within codelist.
-    This is achived using get_val method of the codeserver instance (see
-    its documentation).
+    A codeserver object, is able to evaluate strings of expressions within
+    the namespace of each client (as local) and code module within the 
+    codelist (as global). This is achived using get_val method of the 
+    codeserver instance (see its documentation).
     """
     
-    def __init__(self, codelist):
+    def __init__(self, codelist, clients):
         self.codelist = codelist
         self.codespace = codespace(codepool(self.codelist))
-
-    def get_val(self, code, expr_string):
+        self.clients = clients
+        self.clients_locals = init_locals(self.clients)
+        self.exps_expr = {}
+   
+    def get_val(self, code, client, expr):
         """
-        expr_string: should be a valid string expression
+        expr: should be a valid string expression
         code: is the string name;  should be in self.codelist. 
-        expr_string gets evaluated within the code environment; the value
-        gets returned
+        expr gets evaluated within the code context as global and
+        the locals of cleint namespace; the value gets returned
         """
         if not (code in self.codelist):
             return "%s is not known in codelist!" % code
+        elif not (client in self.clients):
+            return "%s is not known in clients list!" % client
+        elif not pattern.match(expr) == None: 
+            return "You can not use import."
         else:
             try: 
-                return eval(expr_string, self.codespace[code].__dict__)
+                return eval(expr, self.codespace[code].__dict__,\
+                            self.clients_locals[client])
             except:
-                return "Somthing wrong with your request!"
+                #try:
+                 exec(expr, self.codespace[code].__dict__,\
+                            self.clients_locals[client])
+                #except:
+                    #return "Somthing's wrong!"
 
     def assert_equal(self, code, exp1, exp2):
         """
@@ -46,4 +69,5 @@ class codeserver():
         returns False
         """
         return self.get_val(code, exp1) == self.get_val(code, exp2)
+
 
