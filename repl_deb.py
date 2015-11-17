f4e8ab1247da133ea4a3ede6f2e48f4688085a20
Mohammad Moghaddam <moghaddam22@gmail.com>
2015-11-17 08:54:22 -0800

    added tests for repl

--- repl_deb.py
+++ repl_deb.py
@@ -1,18 +1,35 @@
 import simple_repl as sr
+import unittest
 
-def main():
-    mo = sr.repl('mo', 1)
-    mo.send(u'a = 1\n')
-    mo.send(u'a\n')
-    mo.send(u'def f():\n')
-    mo.send(u'print a\n')
-    mo.send(u'\n')
-    mo.send(u'a\n')
+class BasicRepl(unittest.TestCase):
 
+    def setUp(self):
+        self.user = sr.repl('user', 1)
+        self.user.send(u'a = 1\n')
+        self.var1 = self.user.send(u'a\n')
 
+    def test_userAssignsValuesToVars(self):    
+        self.assertEqual(self.var1, '1\n>>> ')
+    
+    def test_userSpaceDoesNotLeakToGlob(self):
+        self.user.send(u'dummy = 4\n')
+        dummy = 3
+        self.assertEqual(self.user.send(u'dummy\n'),  '4\n>>> ')
+    
+    def test_replBufersNonCompleteLines(self):
+        self.assertEqual(self.user.send(u'def f():\n'), '... ')
+        dummy = 3
+        o = self.user.send(u'    print dummy\n')
+        self.assertTrue(self.user.bufer[0] == u'def f():\n')
+        self.assertTrue(self.user.bufer[1] == u'    print dummy\n')
+        self.assertEqual(o, '... ')
+        o = self.user.send(u'\n')
+        self.assertEqual(len(self.user.bufer), 0)
+        self.assertTrue(o == '>>> ')
+        self.assertEqual(self.user.send(u'f()\n'), '4\n>>> ')
 
 if __name__ == '__main__':
-    main()
+    unittest.main()
 
 
 
